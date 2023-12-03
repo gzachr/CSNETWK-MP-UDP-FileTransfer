bc4e5e3d0e8eb94a55cbc103f189045ef7659c91
gzachr <zachary_gomez@dlsu.edu.ph>
2023-12-03 15:33:29 +0800

    implemented join, register, and leave

--- Client.py
+++ Client.py
@@ -5,52 +5,101 @@ import socket
 import json
 import threading
 
-def send_message(message, server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serialized_message = json.dumps(message).encode('utf-8')
-    client_socket.sendto(serialized_message, server_address)
+server_address = None
 
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print("Welcome")
-    print("Enter /? for help")
+def display_commands():
+    print('\nValid Commands:')
+    print('/? - Display commands')
+    print('/join <server address> - Join a server')
+    print('/register <handle> - Register a handle to the serer')
+    print('/store <filename> - Store a file')
+    print('/get <filename> - Retrieve a file from the server')
+    print('/dir - List all stored files on the server')
+    print('/msg <handle> <message> - Send a message to a client')
+    print('/all <message> - Send a message to all clients')
+    print('/leave - Leave the server\n')
+
+# receive messages from server
+def receive_response():
+    global server_address
 
     while True:
-        user_input = input("Enter command: ")
+        if server_address is not None:
+            try:
+                data, addr = client_socket.recvfrom(1024)
+
+                if not data:
+                    break
+
+                response = json.loads(data.decode())
+
+                if response['command'] == 'join' or response['command'] == 'error':
+                        print(f"{response['message']}\n")
+                
+                elif response['command'] == 'leave':
+                    print(f"{response['message']}\n")
+                    server_address = None  
+                
+                elif response['command'] == 'register':
+                    print(f"Welcome {response['client']}!")
+                    print(f"{response['message']}\n")
+
+                # do message based on other responses
+            
+            except json.JSONDecodeError:
+                print('Error decoding JSON data from the server.\n')
+        
+
+def start():
+    while True:
+        user_input = input()
         parts = user_input.split()
         command = parts[0]
         params = parts[1:]
 
-        # show valid commands
+        message = {'command': command, 'params': params}
+
         if command == '/?':
             if len(params) == 0:
-                print("""Valid Commands:
-                        /join <ip> <port> - Join a network
-                        /register <username> - Register a user
-                        /store <filename> - Store a file
-                        /dir - Show a list of all files in the server
-                        /get <filename> - Download a file from the server
-                        /? - Show this help message
-                        """)
+                display_commands()
             else:
-                print("Error: Command parameters do not match or is not allowed.")
-            
+                print('Error: Command parameters do not match or is not allowed.\n')
+
         elif command == '/join':
-            # succesful connection
             if len(params) == 2 and (params[0] =='127.0.0.1' or params[0] == 'localhost') and params[1] == '12345':
-                server_address = (params[0], int(params[1]))
-                
-                sock.connect(server_address)
-                # insert rest of code here
+                temp = (params[0], int(params[1]))
+                client_socket.sendto(json.dumps(message).encode('utf-8'), temp)
+
+                global server_address 
+                server_address = temp
             else:
-                print("Error: Connection to the Server has failed! Please check IP Address and Port Number.")
+                print('Error: Connection to the Server has failed! Please check IP Address and Port Number.\n')
+        
+        elif command == '/register' or command == '/store' or command == '/get' or command == '/dir' or command == '/msg' or command == '/all':
+            if server_address:
+                client_socket.sendto(json.dumps(message).encode('utf-8'), server_address)
+            else:
+                print('Error: Please connect to the server first.\n')
         
         elif command == '/leave':
-            print("Error: Disconnection failed. Please connect to the server first.")
-
-        else:
-            print("Error: Command not found.")
+            if server_address:
+                client_socket.sendto(json.dumps(message).encode('utf-8'), server_address)
+            else:
+                print('Error: Disconnection failed. Please connect to the server first.\n')
         
+        else:
+            print('Error: Command not found.\n')
 
-if __name__ == "__main__":
-    main()
+# UDP Socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#threads
+receive_thread = threading.Thread(target=receive_response)
+
+receive_thread.start()
+
+print("Input /? to view possible commands")
+print("Enter command: ")
+start()
+
+    
--- Server.py
+++ Server.py
@@ -3,68 +3,115 @@
 # CSNETWK S12 MP
 import socket
 import json
-import threading
 
-server_ip = '127.0.0.1'
-server_port = 12345
+clients = {}
 
-def send_message(message, server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serialized_message = json.dumps(message).encode('utf-8')
-    client_socket.sendto(serialized_message, server_address)
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# handles the /join command
-def join(params, addr):
-    pass
+# Bind the socket to a specific IP address and port
+server_address = ('127.0.0.1', 12345)
+sock.bind(server_address)
 
-# handles the /register command
-def register(params, addr):
-    pass
+print('Server started at {} port {}'.format(*server_address))
 
-# handles the /store command
-def store(params, addr):
-    pass
+while True:
+    data, address = sock.recvfrom(1024)
 
-# handles the /dir command
-def dir(params, addr):
-    pass
+    try:
+        response = json.loads(data.decode())
+    
+    except json.JSONDecodeError as e:
+        message = {'command': 'error', 'message': str(e)}
+        sock.sendto(json.dumps(message).encode('utf-8'), address)
+        continue
 
-# handles the /get command
-def get(params, addr):
-    pass
+    # handle joining
+    if response['command'] == '/join':
+        print(f"Client {address} connected to server")
+        message = {'command': 'join', 'message': "Connection to the File Exchange Server is successful!"}
+        sock.sendto(json.dumps(message).encode('utf-8'), address)
 
-# handles the /? command
-def help(params, addr):
-    pass
+    # handle registration
+    elif response['command'] == '/register':
+        # check num of params
+        if len(response['params']) != 1:
+            message = {'command': 'error', 'message': 'Error: Command parameters do not match or is not allowed.\n'}
+            sock.sendto(json.dumps(message).encode('utf-8'), address)
 
-command_list = {
-    '/join': join,
-    '/register': register,
-     '/store': store,
-     '/dir': dir,
-     '/get': get,
-     '/?': help,
-}
-
-def parse_message(message_str):
-    message = json.loads(message_str)
-    return message["command"], message["params"]
-
-def main():
-    #UDP socket
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    sock.bind((server_ip, server_port))
-
-    while True:
-        data, addr = sock.recvfrom(1024)
-        message_str = data.decode('utf-8')
-        command, message = parse_message(message_str)
-
-        if command in command_list:
-            command_list[command](message)(addr)
         else:
-            send_message({'message': 'Error: Command not found.'}, addr)
+            handle = response['params'][0]
+            flag = True
+            for key, value in clients.items():
+
+                # check if handle is unique
+                if key == handle:
+                    message = {'command': 'error', 'message': 'Error: Registration failed. Handle or alias already exists.\n'}
+                    sock.sendto(json.dumps(message).encode('utf-8'), address)
+                    flag = False
+                    break
+
+                # check if address is unique
+                elif value == address:
+                    message = {'command': 'error', 'message': 'Error: Registration failed. Address is already registered.\n'}
+                    sock.sendto(json.dumps(message).encode('utf-8'), address)
+                    flag = False
+                    break
+
+            # register handle           
+            if flag:
+                clients[handle] = address
+                print(f"Client {address} registered as {handle}")
+                message = {'command': 'register', 'client':  handle, 'message': 'You have successfully registered.'}
+                sock.sendto(json.dumps(message).encode('utf-8'), address)
         
-if __name__ == "__main__":
-    main()
+        print(clients)
+    
+    # handle leaving
+    elif response['command'] == '/leave':
+        # check num of params
+        if len(response['params']) != 0:
+            message = {'command': 'error', 'message': 'Error: Command parameters do not match or is not allowed.\n'}
+            sock.sendto(json.dumps(message).encode('utf-8'), address)
+
+        else:
+            # remove client from list if registered
+            for key, value in clients.items():
+                if value == address:
+                    print(f"Client {key} left the server")
+                    clients.pop(key)
+                    break
+
+            # notify client of disconnection
+            print(f"{address} has disconnected")
+            message = {'command': 'leave', 'message': f"Connection closed. Thank you!"}
+            sock.sendto(json.dumps(message).encode('utf-8'), address)
+    
+    # handle list of files (dir)
+    elif response['command'] == '/dir':
+        pass
+
+    # handle getting of file
+    elif response['command'] == '/get':
+        pass
+
+    # handle storing of file
+    elif response['command'] == '/store':
+        pass
+
+    # handle message to single client
+    elif response['command'] == '/msg':
+        pass
+
+    # handle message to all clients
+    elif response['command'] == '/all':
+        pass
+
+    # if command does not exist
+    else:
+        message = {'command': 'error', 'message': 'Error: Command not found.\n'}
+        sock.sendto(json.dumps(message).encode('utf-8'), address)
+
+
+    
 
